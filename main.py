728ea05fd8b2f87782764f14f5e5daacefab1c95
Bouncyyahomie <thornny.s@gmail.com>
2023-04-18 04:54:30 +0700

    fix

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ import random
 import csv
 from io import BytesIO
 import tensorflow as tf
-import tensorrt as trt
+# import tensorrt as trt
 
-trt.init_libnvinfer_plugins(None,'')
+# trt.init_libnvinfer_plugins(None,'')
 
 def set_seed(seed):
   os.environ['TF_DETERMINISTIC_OPS'] = '1'
